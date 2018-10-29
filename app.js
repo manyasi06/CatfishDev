0dc0b1bafd5b35d86b15b463becb445338157cf5
Bryan Musungu <bryanmusungu@Bryans-MacBook-Pro.local>
2018-10-28 19:07:57 -0500

    setting up handlebars tables test

--- app.js
+++ app.js
@@ -53,8 +53,37 @@ app.set('port', process.env.PORT || 3010);
 //       });
 //     });
 //   });
-app.get('/',function(req,res){
-    res.render('home');
+app.get('/',function(req,res,next){
+    var context = {};
+    mysql.pool.query('SELECT NCBI_ProteinID, ProteinIDB, Annotation, Organism  FROM ' 
+    + 'GeneID as a inner join Ortholog as b On a.NCBI_ProteinID =b.ProteinIDA', function(err,rows,fields){
+        if(err){
+            next(err);
+            return;
+        }
+        //console.log(JSON.stringify(rows));
+        //console.log(JSON.stringify(fields));
+        row_data = {};
+        row_data.inters = [];
+        for( row in rows){
+            inter = {};
+
+            inter.ProteinID_A = rows[row].NCBI_ProteinID;
+            inter.ProteinID_B = rows[row].ProteinIDB;
+            inter.PA_annotation = rows[row].Annotation;
+            inter.Organism = rows[row].Organism;
+            
+            row_data.inters.push(inter);
+        }
+        //make data into json
+        context.results = JSON.stringify(rows);
+        context.data = row_data;
+
+        console.log('This is the results:\n' + context.data);
+        res.render('home', context);
+
+    })
+    
     //res.send("Home page");
 });
   
