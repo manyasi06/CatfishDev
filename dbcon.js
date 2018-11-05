e427c2086a778d26ce3be10a8061e6ada38ff87f
Bryan Musungu <bryanmusungu@10-197-36-84.sds.oregonstate.edu>
2018-11-04 23:13:49 -0600

    Fix server for submission

--- dbcon.js
+++ dbcon.js
@@ -1,12 +1,13 @@
 var mysql = require('mysql');
-// var pool = mysql.createPool({
-//   connectionLimit : 10,
-//   host            : 'classmysql.engr.oregonstate.edu',
-//   user            : 'cs340_musungub',
-//   password        : '1630',
-//   database        : 'cs340_musungub'
-// });
+var pool = mysql.createPool({
+  connectionLimit : 10,
+  host            : 'classmysql.engr.oregonstate.edu',
+  user            : 'cs340_musungub',
+  password        : '1630',
+  database        : 'cs340_musungub'
+});
 
+/*
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'localhost',
@@ -15,6 +16,7 @@ var pool = mysql.createPool({
     database        : 'CatfishDatabase',
     multipleStatements: true
   });
+  */
 
 
 module.exports.pool = pool;
