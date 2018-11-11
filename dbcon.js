86d8e68c3e4b51f1d2d07a3d1dc43590ed219eb6
manyasi06 <musungub@oregonstate.edu>
2018-11-11 14:16:40 -0800

    add the database

--- dbcon.js
+++ dbcon.js
@@ -1,12 +1,12 @@
 var mysql = require('mysql');
-/*var pool = mysql.createPool({
+var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
   user            : 'cs340_musungub',
   password        : '1630',
   database        : 'cs340_musungub'
 });
-*/
+/*
 
 var pool = mysql.createPool({
     connectionLimit : 10,
@@ -17,7 +17,7 @@ var pool = mysql.createPool({
     multipleStatements: true
   });
   
-/*
+
 var pool = mysql.createPool({
     connectionLimit : 10,
     host            : 'localhost',
